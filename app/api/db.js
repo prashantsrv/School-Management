ebad5a434c57c9ea01e26749808e6fc12a7452ce
Prashant Srivastav <prashant07sr@gmail.com>
2025-12-03 21:19:37 +0530

    Revert database file to previous state

--- app/api/db.js
+++ app/api/db.js
@@ -4,15 +4,12 @@ import mysql from "mysql2/promise";
 
 export async function connectDB() {
   try{
-  // const db = await mysql.createConnection({
-    // host: "localhost",
-    // user: "root",
-    // password: "vishu",
-    // database: "mydb",
-    
-  // });
-  // return db;
-  const db = await mysql.createConnection(process.env.DATABASE_URL);
+  const db = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "vishu",
+    database: "mydb",
+  });
   return db;
   }catch(error){
     console.log("database conn failed", error.message)
